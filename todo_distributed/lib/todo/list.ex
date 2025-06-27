553732c0316ef726b1a21be4a2cafc6ff5545594
adam vietro <hunterafi@gmail.com>
2025-06-26 17:03:33 -0700

    Todo_Distributed Finished changes to the todo_distributed.

--- todo_distributed/lib/todo/list.ex
+++ todo_distributed/lib/todo/list.ex
@@ -1,31 +1,22 @@
 defmodule Todo.List do
   defstruct next_id: 1, entries: %{}
 
-  # def new(), do: %TodoList{}
-
   def new(entries \\ []) do
-    Enum.reduce(entries, %Todo.List{}, fn entry, todo_list ->
-      add_entry(todo_list, entry)
-    end)
+    Enum.reduce(
+      entries,
+      %Todo.List{},
+      &add_entry(&2, &1)
+    )
+  end
 
-    # entires, %TodoList{}, &add_entry(&2, &1)
-    # Needs to be in this order as the lambda will pass the entry then the acc.
+  def size(todo_list) do
+    map_size(todo_list.entries)
   end
 
   def add_entry(todo_list, entry) do
-    # adds the id to the entry
     entry = Map.put(entry, :id, todo_list.next_id)
+    new_entries = Map.put(todo_list.entries, todo_list.next_id, entry)
 
-    new_entries =
-      Map.put(
-        todo_list.entries,
-        todo_list.next_id,
-        entry
-      )
-
-    # adds the new entry to the todo_list
-
-    # updates the next id
     %Todo.List{todo_list | entries: new_entries, next_id: todo_list.next_id + 1}
   end
 
@@ -35,12 +26,7 @@ defmodule Todo.List do
     |> Enum.filter(fn entry -> entry.date == date end)
   end
 
-  def entries(todo_list) do
-    todo_list.entries
-    |> Map.values()
-  end
-
-  def update_entries(todo_list, entry_id, updater_fun) do
+  def update_entry(todo_list, entry_id, updater_fun) do
     case Map.fetch(todo_list.entries, entry_id) do
       :error ->
         todo_list
@@ -53,14 +39,6 @@ defmodule Todo.List do
   end
 
   def delete_entry(todo_list, entry_id) do
-    case Map.fetch(todo_list.entries, entry_id) do
-      :error ->
-        IO.inspect("cant find")
-        todo_list
-
-      {:ok, _entry} ->
-        updated_entries = Map.delete(todo_list.entries, entry_id)
-        %Todo.List{todo_list | entries: updated_entries}
-    end
+    %Todo.List{todo_list | entries: Map.delete(todo_list.entries, entry_id)}
   end
 end
