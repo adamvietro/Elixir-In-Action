0bfe44d1bd50dbe31168e9edeec21e3c264f0d5f
adam vietro <hunterafi@gmail.com>
2025-06-29 13:05:56 -0700

    Finished chapter 13 Done with the book now I need to update the README.md and then find an other book.

--- todo_distributed/mix.exs
+++ todo_distributed/mix.exs
@@ -19,7 +19,7 @@ defmodule Todo.MixProject do
 
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :runtime_tools],
       mod: {Todo.Application, []}
     ]
   end
